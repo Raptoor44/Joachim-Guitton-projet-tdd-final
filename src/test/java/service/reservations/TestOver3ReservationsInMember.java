b37a8e74e1be9d0a109cbbeae4610c4ae085a8e2
Joachim Guitton <joachim.guitton@gmail.com>
2024-04-28 19:00:00 +0200

    feature : Mise en place du test qui permet de lever une exception quand un membre dépasse 3 réservations.

---
+++ src/test/java/service/reservations/TestOver3ReservationsInMember.java
@@ -0,0 +1,27 @@
+package service.reservations;
+
+import org.example.models.Reservation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestOver3ReservationsInMember extends ReservationServiceTestAbstract{
+
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+    }
+
+    @Test
+    public void testOver3ReservationsInMember() {
+        assertThrows(Over3ReservationsInMemberException.class, this.insert3ReservationsInMember());
+    }
+
+    public void insert3ReservationsInMember(){
+        for(int i = 0; i<4; i++){
+            Reservation r = new Reservation();
+            this.member.addReservation(r);
+        }
+    }
+}
