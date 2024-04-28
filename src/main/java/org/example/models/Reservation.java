0953548929300429450d8d25a52d0ce5f9fe64c1
Joachim Guitton <joachim.guitton@gmail.com>
2024-04-28 18:54:03 +0200

    feature : Mise en place du test de la date limite de réservation + implémentation de la fonctionnalité dans la classe Reservation.

--- src/main/java/org/example/models/Reservation.java
+++ src/main/java/org/example/models/Reservation.java
@@ -1,4 +1,44 @@
 package org.example.models;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Reservation {
+
+    private Date dateLimit;
+
+    private Date dateCreation;
+
+    public Reservation() {
+        this.dateCreation = new Date();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 4);
+
+        this.dateLimit = calendar.getTime();
+    }
+
+    public Date getDateLimit() {
+        return dateLimit;
+    }
+
+    public void setDateLimit(Date dateLimit) {
+        this.dateLimit = dateLimit;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public boolean isValid(Date dateToday){
+       if(dateToday.compareTo(this.dateLimit) <= 0){
+           return true;
+       }else{
+           return false;
+       }
+    }
 }
---
+++ src/test/java/service/reservations/TestDateLimitReservation.java
@@ -0,0 +1,35 @@
+package service.reservations;
+
+import org.example.models.Reservation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDateLimitReservation extends ReservationServiceTestAbstract {
+
+    private Date dateTimeToday;
+
+    private Reservation reservation;
+
+    @BeforeEach
+    public void setUp() {
+
+        //Nous ajout 5 mois à la date d'aujourd'hui
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 5);
+
+        this.dateTimeToday = calendar.getTime();
+
+        // Mock pour la méthode isValid()
+        this.reservation = new Reservation();
+    }
+
+    @Test
+    public void testDateLimitReservation() {
+        assertEquals(false, reservation.isValid(dateTimeToday));
+    }
+}
